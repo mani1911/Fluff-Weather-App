600aac499a1d66a20d990695d00e6623f6ec5bfc
mani1911 <mail2aj19@gmail.com>
2022-12-09 19:43:24 +0530

    feat(App): Complete UI

--- lib/screens/weather_app.dart
+++ lib/screens/weather_app.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:transformer_page_view/transformer_page_view.dart';
+import 'package:weather_app/models/weather_locations.dart';
+import 'package:weather_app/widgets/single_widget.dart';
+import 'package:weather_app/widgets/slider.dart';
+import 'package:weather_app/widgets/transform.dart';
 
-class WeatherApp extends StatelessWidget {
+class WeatherApp extends StatefulWidget {
+  @override
+  State<WeatherApp> createState() => _WeatherAppState();
+}
+
+class _WeatherAppState extends State<WeatherApp> {
+  int _currentPage = 0;
+  late String bgImg;
+
+  _onPageChange(int index) {
+    setState(() {
+      _currentPage = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (locationList[_currentPage].weatherType == 'Sunny') {
+      bgImg = 'assets/sunny.jpg';
+    } else if (locationList[_currentPage].weatherType == 'Night') {
+      bgImg = 'assets/night.jpg';
+    } else if (locationList[_currentPage].weatherType == 'Rainy') {
+      bgImg = 'assets/rainy.jpg';
+    } else if (locationList[_currentPage].weatherType == 'Cloudy') {
+      bgImg = 'assets/cloudy.jpeg';
+    }
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: const Text('Weather App'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         leading: IconButton(
           icon: Icon(
             Icons.search,
@@ -18,15 +48,48 @@ class WeatherApp extends StatelessWidget {
             print('Pressed');
           },
         ),
+        actions: [
+          Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 20, 0),
+              child: GestureDetector(
+                onTap: () => {print('Tapped')},
+                child: SvgPicture.asset(
+                  'assets/menu.svg',
+                  height: 30,
+                  width: 30,
+                  color: Colors.white,
+                ),
+              )),
+        ],
       ),
       body: Stack(
         children: [
           Image.asset(
-            'assets/night.jpg',
+            bgImg,
             fit: BoxFit.cover,
             height: double.infinity,
             width: double.infinity,
-          )
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.black38,
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.only(top: 140, left: 15),
+              child: Row(
+                children: [
+                  for (int i = 0; i < locationList.length; i++)
+                    if (i == _currentPage) SliderDot(true) else SliderDot(false)
+                ],
+              )),
+          TransformerPageView(
+            transformer: ScaleAndFadeTransformer(),
+            onPageChanged: _onPageChange,
+            itemCount: locationList.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (ctx, i) => SingleWeather(i),
+          ),
         ],
       ),
     );
